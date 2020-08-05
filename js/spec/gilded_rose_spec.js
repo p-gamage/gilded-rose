af57d2bfe23735e0a2d7fe1105dfee69d7cd5e82
Priyanga Gamage <pgamag3@gmail.com>
2020-08-05 19:11:28 +0100

    added tests for basic functionality

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,7 +1,21 @@
 describe("Gilded Rose", function () {
-  it("should foo", function () {
-    items = [new Item("foo", 0, 0)];
-    update_quality();
-    expect(items[0].name).toEqual("foo");
+  describe("basic functionality", () => {
+    beforeEach(() => {
+      const normalItem = new Item("normalItem", 5, 10);
+      items.push(normalItem);
+      update_quality();
+    });
+
+    afterEach(() => {
+      items = [];
+    });
+
+    it("lowers the quality of a normal item by 1", function () {
+      expect(items[0].quality).toEqual(9);
+    });
+
+    it("lowers the sell in value of a normal item by 1", function () {
+      expect(items[0].sell_in).toEqual(4);
+    });
   });
 });

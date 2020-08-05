1e339203d2fe0fb66167ef9602932c07594b17ac
Priyanga Gamage <pgamag3@gmail.com>
2020-08-06 00:37:02 +0100

    added tests to cover all scenarios

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,4 +1,8 @@
 describe("Gilded Rose", () => {
+  const BRIE = "Aged Brie";
+  const SULFURAS = "Sulfuras, Hand of Ragnaros";
+  const BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+
   afterEach(() => {
     items = [];
   });
@@ -41,27 +45,27 @@ describe("Gilded Rose", () => {
 
   describe("increases quality of certain items when they age", () => {
     it("by 1 value for items within sell in date", () => {
-      items.push(new Item("Aged Brie", 1, 3));
+      items.push(new Item(BRIE, 1, 3));
       update_quality();
       expect(items[0].quality).toEqual(4);
     });
 
     it("by 2 values for items sell in date", () => {
-      items.push(new Item("Aged Brie", -1, 3));
+      items.push(new Item(BRIE, -1, 3));
       update_quality();
       expect(items[0].quality).toEqual(5);
     });
   });
 
   it("never increases the quality of an item more than 50", () => {
-    items.push(new Item("Aged Brie", 3, 50));
+    items.push(new Item(BRIE, 3, 50));
     update_quality();
     expect(items[0].quality).toEqual(50);
   });
 
   describe("legendary items", () => {
     beforeEach(() => {
-      items.push(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
+      items.push(new Item(SULFURAS, -1, 80));
       update_quality();
     });
 
@@ -73,5 +77,48 @@ describe("Gilded Rose", () => {
       update_quality();
       expect(items[0].sell_in).toEqual(-1);
     });
+  });
+
+  describe("Backstage passes", () => {
+    const testCases = [
+      {
+        days: 11,
+        increase: 1,
+      },
+      {
+        days: 17,
+        increase: 1,
+      },
+      {
+        days: 10,
+        increase: 2,
+      },
+      {
+        days: 7,
+        increase: 2,
+      },
+      {
+        days: 5,
+        increase: 3,
+      },
+      {
+        days: 3,
+        increase: 3,
+      },
+    ];
+
+    testCases.forEach((testCase) => {
+      it(`increases value by ${testCase.increase} when ${testCase.days} days old`, () => {
+        items.push(new Item(BACKSTAGE_PASS, testCase.days, 0));
+        update_quality();
+        expect(items[0].quality).toEqual(testCase.increase);
+      });
+    });
+  });
+
+  it("drops quality to 0 after concert", () => {
+    items.push(new Item(BACKSTAGE_PASS, 0, 5));
+    update_quality();
+    expect(items[0].quality).toEqual(0);
   });
 });

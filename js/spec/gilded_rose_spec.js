000e7f47509964caff23fa45d72c418b24e626a7
Priyanga Gamage <pgamag3@gmail.com>
2020-08-05 18:10:15 +0100

    fixed the failing test

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,7 @@
-describe("Gilded Rose", function() {
-
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+describe("Gilded Rose", function () {
+  it("should foo", function () {
+    items = [new Item("foo", 0, 0)];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
-
 });
